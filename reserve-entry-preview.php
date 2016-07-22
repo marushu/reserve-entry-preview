4e41815c2acd448b0b587bdef983f3b0aa3eba79
vagrant <vagrant@vccw.dev>
2016-07-22 09:26:33 +0000

    first commit

---
+++ reserve-entry-preview.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Reserve-entry-preview
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: reserve-entry-preview
+ * Domain Path: /languages
+ * @package Reserve-entry-preview
+ */
